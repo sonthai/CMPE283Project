3b469cbceab10061dcf5f6cc58ea078b6502ef37
archerleonard@gmail.com <leonardlee@Leonards-MacBook-Pro.local>
2016-11-29 22:20:52 -0800

    finish registration restful api

--- server/src/main/java/com/dashboard/services/UserServices.java
+++ server/src/main/java/com/dashboard/services/UserServices.java
@@ -2,12 +2,18 @@ package com.dashboard.services;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 
+import java.util.Date;
 import java.util.Map;
 
 public class UserServices {
     private static final Logger log = LoggerFactory.getLogger(UserServices.class);
+
+    @Autowired
+    NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
     public int login(Map<String, String> data) {
         int result = 0;
@@ -18,6 +24,9 @@ public class UserServices {
     public int register(Map<String, String> data) {
         int result = 0;
 
+        String sql = "Insert into User (first_name, last_name, account, pwd, email) ";
+        sql += "values (:first_name, :last_name, :account, :pwd, :email)";
+        namedParameterJdbcTemplate.update(sql, data);
         return result;
     }
 }
