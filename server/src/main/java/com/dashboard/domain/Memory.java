a6f56aeb6243c9721963b1cda8069d412860e133
KevinX Wang <516878610@qq.com>
2016-12-06 15:59:56 -0800

    add billing related domain files

---
+++ server/src/main/java/com/dashboard/domain/Memory.java
@@ -0,0 +1,25 @@
+package com.dashboard.domain;
+
+/**
+ * Created by kevin on 2016/12/6.
+ */
+public class Memory {
+    private int id;
+    private int size;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+}
---
+++ server/src/main/java/com/dashboard/domain/Storage.java
@@ -0,0 +1,26 @@
+package com.dashboard.domain;
+
+/**
+ * Created by kevin on 2016/12/6.
+ */
+public class Storage {
+
+    private int id;
+    private int size;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+}
